c414a29a52ea1e949f066cfe52ed441092a7b49c
Kedar Deshpande <61658388+kedar17102001@users.noreply.github.com>
2024-03-14 15:23:20 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Welcome to Unnati Development..... !!!! Bye";
+    return "Welcome to KUCL2.0 Course..... !!!! Bye";
   }
 }
